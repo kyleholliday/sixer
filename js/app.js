6d460530a8f18e4769fd7ca6c6ecb2cda9afe1b2
Brad Runnels <runnels.brad@gmail.com>
2016-04-03 14:35:31 -0400

    handling functionality for our landing page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,44 @@
 
 var _ = require('underscore');
 var Firebase = require('firebase');
-var addBeer = require('./addBeer');
-var getBeer = require('./getBeer');
 
 window.addEventListener('load', function() {
 
-  getBeer();
-  
+   // html template used to inject our data
+   var beerTemplate = _.template(document.getElementById('beer-template').textContent);
+
+   // get data from Firebase
+   var pull = new Firebase('https://sixer.firebaseio.com/beer/');
+   pull.once('value', function(data) {
+
+      var idList = [];
+      // our data from Firebase
+      var beer = data.val();
+      // iterate over object to get array of ids      
+      for (var prop in beer) {
+         idList.push(prop);
+      }
+
+      for (var i = 0; i < idList.length; i++) {
+         var beers = beerTemplate({
+            name:    beer[idList[i]].name,
+            brewery: beer[idList[i]].brewery,
+            style:   beer[idList[i]].style
+         });
+
+         // creating the elements for display of beer
+         var create = document.createElement('div');
+         create.classList.add('data');
+
+         //Set the ID
+         create.setAttribute('id', 'beer-' + beer[idList[i]].id);
+         create.innerHTML = beers;
+         var parent = document.getElementById('results');
+         parent.appendChild(create);
+      }
+
+   });
+/*
   var search = document.getElementById('search-box');
   search.addEventListener('keyup', function() {
     var text = search.value;
@@ -28,9 +59,6 @@ window.addEventListener('load', function() {
       helper: "clone",
       containment: 'window',
     });
-  });
-
-  // add beer submit button on create page (create.html) writes form data to firebase
-  document.getElementById('btn-add-beer').addEventListener('click', addBeer.add);
+  });*/
 
 });
